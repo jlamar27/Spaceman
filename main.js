ebb3481b1c207ad857048893ba6397e97dc0ca61
jlamar27 <jlamar2712@gmail.com>
2023-08-16 17:26:18 -0400

    added difficulty level

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 import { pokemonArray } from "./pokemon.js";
 
+// Messages
 const GAME_OVER_MESSAGE = "Game Over";
 const CONGRATULATIONS_MESSAGE = "Congratulations! You won!";
 const YOU_LOST_MESSAGE = "You Lost!";
@@ -7,124 +8,115 @@ const GUESSES_REMAINING_MESSAGE = "Guesses Remaining: ";
 const POKEMON_WAS_MESSAGE = "The Pokemon was ";
 const YOU_CAUGHT_MESSAGE = "You Caught ";
 const BETTER_LUCK_NEXT_TIME_MESSAGE = ", better luck next time!";
-const LOST_GIF_URL =
-  "https://media.tenor.com/fKJxAaJdH48AAAAd/pokemon-ash-ketchum.gif";
-const WON_GIF_URL =
-  "https://media.tenor.com/lAz1WcGbKukAAAAC/pokeball-catch.gif";
 
-//wordblanks & letters & guess features & remaining guesses
+// GIF URLs
+const LOST_GIF_URL = "https://media.tenor.com/fKJxAaJdH48AAAAd/pokemon-ash-ketchum.gif";
+const WON_GIF_URL = "https://media.tenor.com/lAz1WcGbKukAAAAC/pokeball-catch.gif";
+
+// Elements
 const wordBlanksContainer = document.querySelector("#wordBlanksContainer");
 const keyboardLetters = document.querySelectorAll("#keyboard .letter");
 const remainingGuessesEl = document.querySelector("#remaining-guesses");
 const guessForm = document.querySelector("#guessForm");
 const guessInput = document.querySelector("#guessInput");
-
-// modal & images
 const modal = document.querySelector("#myModal");
 const closeBtn = document.querySelector(".close");
 const modalMessage = document.querySelector("#modal-message");
 const modalWord = document.querySelector("#modal-word");
 const tryAgainButton = document.querySelector("#try-again-button");
+const difficultySelect = document.querySelector("#difficultySelect"); 
+const guessHistoryContainer = document.querySelector("#guessHistory");
 
-let words = pokemonArray;
-
+// Game state
 let randomWord;
 let randomLetters;
 let remainingGuesses;
 let gameOver;
+let difficulty = 1; // Default difficulty level (1: Easy, 2: Medium, 3: Hard)
 
-// Event listener for the close button in the modal
-closeBtn.addEventListener("click", () => {
-  hideModal();
-  newGame();
-});
-
-// Event listener for the try again button in the modal
-tryAgainButton.addEventListener("click", () => {
-  hideModal();
-  newGame();
-});
-
+// Event listeners
+closeBtn.addEventListener("click", resetAndHideModal);
+tryAgainButton.addEventListener("click", resetAndHideModal);
 modal.addEventListener("keydown", (event) => {
   if (event.key === "Enter") {
-    hideModal();
-    newGame();
+    resetAndHideModal();
   }
 });
+guessForm.addEventListener("submit", handleGuessSubmission);
+guessInput.addEventListener("keydown", handleEnterKey);
+difficultySelect.addEventListener("change", handleDifficultyChange);
 
+// Initialize the game
+newGame();
 
-// Event listener for the guess form submission
-guessForm.addEventListener("submit", function (event) {
-  event.preventDefault(); // Prevent form submission
+function resetAndHideModal() {
+  hideModal();
+  newGame();
+  guessHistoryContainer.textContent = "";
+}
 
+function handleGuessSubmission(event) {
+  event.preventDefault();
   const userGuess = guessInput.value.trim().toUpperCase();
   const isValidInput = /^[A-Za-z]+$/.test(userGuess);
-  
-  if (isValidInput) {
-    checkWin(userGuess);
-  } else {
-    // Handle invalid input
-  }
-});
 
-
-// Event listener for the Enter key press
-guessInput.addEventListener("keydown", function (event) {
-  if (event.key === "Enter") {
-    event.preventDefault(); // Prevent form submission
-    const userGuess = guessInput.value.trim().toUpperCase();
-    const isValidInput = /^[A-Za-z]+$/.test(userGuess);
-
-  if (modal.style.display === "block") {
-    hideModal();
-    newGame();
+  if (!isValidInput) {
+    alert("Please enter only letters.");
     return;
   }
 
+  if (modal.style.display === "block") {
+    resetAndHideModal();
+    return;
+  }
+
+  checkWin(userGuess);
+  addToGuessHistory(userGuess);
+}
+
+function handleEnterKey(event) {
+  if (event.key === "Enter") {
+    event.preventDefault();
+    const userGuess = guessInput.value.trim().toUpperCase();
+    const isValidInput = /^[A-Za-z]+$/.test(userGuess);
+
+    if (modal.style.display === "block") {
+      resetAndHideModal();
+      return;
+    }
+
     if (isValidInput) {
       const isGuessCorrect = checkWin(userGuess);
-      if (isGuessCorrect) {
-        // Handle win
-        return; 
-      } else {
-        incorrectGuess()
+      if (!isGuessCorrect) {
+        incorrectGuess();
       }
-    } else {
-      // Handle invalid input
     }
   }
-});
-
-
-
-
-
+}
 
 function newGame() {
-  const pokemonNames = pokemonArray.map((obj) => obj.name);
+  const pokemonNames = pokemonArray.map(obj => obj.name);
   const randomIndex = Math.floor(Math.random() * pokemonNames.length);
   randomWord = pokemonNames[randomIndex].toUpperCase();
-  console.log(randomWord);
   randomLetters = randomWord.split("");
-  console.log(randomLetters);
-
-  remainingGuesses = 6;
+  setDifficulty();
   remainingGuessesEl.textContent = `${GUESSES_REMAINING_MESSAGE}${remainingGuesses}`;
 
-  keyboardLetters.forEach((letterElement) => {
+  keyboardLetters.forEach(letterElement => {
     letterElement.style.pointerEvents = "auto";
     letterElement.style.opacity = 1;
   });
 
-  wordBlanksContainer.innerHTML = "";
+  wordBlanksContainer.innerHTML = '';
 
   for (let i = 0; i < randomLetters.length; i++) {
     let blank = document.createElement("span");
     blank.classList.add("word-blank");
+    blank.textContent = "";
     wordBlanksContainer.appendChild(blank);
   }
 
-  document.querySelectorAll(".letter").forEach((letter) => {
+  keyboardLetters.forEach(letter => {
     letter.addEventListener("click", letterClick);
   });
 
@@ -133,12 +125,12 @@ function newGame() {
   const hintTextElement = document.getElementById("hintText");
   hintTextElement.textContent = hint;
 
-  guessForm.guessInput.value = "";
+  guessInput.value = "";
 }
 
 function letterClick() {
-  let clickedLetter = this.textContent;
-  let blanks = document.querySelectorAll(".word-blank");
+  const clickedLetter = this.textContent;
+  const blanks = document.querySelectorAll(".word-blank");
 
   let found = false;
 
@@ -149,96 +141,100 @@ function letterClick() {
     }
   }
 
-  if (found) {
-    this.style.pointerEvents = "none";
-    this.style.opacity = 0;
+  this.style.pointerEvents = "none";
+  this.style.opacity = 0;
 
-    const allLettersGuessed = Array.from(blanks).every(blank => blank.textContent !== "");
-    if (allLettersGuessed) {
-      document.querySelectorAll(".letter").forEach((letter) => {
-        letter.removeEventListener("click", letterClick);
-      });
-      checkWin(randomWord);
-    }
-  } else {
-    this.style.pointerEvents = "none";
-    this.style.opacity = 0;
+  const allLettersGuessed = Array.from(blanks).every(blank => blank.textContent !== "");
+  if (allLettersGuessed) {
+    keyboardLetters.forEach(letter => {
+      letter.removeEventListener("click", letterClick);
+    });
+    checkWin(randomWord);
+  } else if (!found) {
     incorrectGuess();
   }
 }
 
-
-// Function for incorrect guesses
 function incorrectGuess() {
   remainingGuesses--;
   remainingGuessesEl.textContent = `${GUESSES_REMAINING_MESSAGE}${remainingGuesses}`;
 
   if (remainingGuesses === 0) {
-    document.querySelectorAll(".letter").forEach((letter) => {
+    keyboardLetters.forEach(letter => {
       letter.removeEventListener("click", letterClick);
     });
 
     remainingGuessesEl.textContent = GAME_OVER_MESSAGE;
 
-    if (guessInput !== randomWord) {
+    if (guessInput.value !== randomWord) {
       showModal(YOU_LOST_MESSAGE, randomWord);
-      newGame()
+      newGame();
     }
+
+    guessHistoryContainer.textContent = "";
   }
 }
 
-// Function to check win condition
 function checkWin(userGuess) {
-  const guessedLetters = document.querySelectorAll(".word-blank");
-  const guessedLetterIds = [...guessedLetters].map(letter => letter.textContent);
+  const guessedLetterIds = Array.from(document.querySelectorAll(".word-blank")).map(letter => letter.textContent);
 
   const isUserGuessCorrect = userGuess === randomWord;
   const areAllLettersGuessed = randomLetters.every(letter => guessedLetterIds.includes(letter));
 
-  if (isUserGuessCorrect) {
-    remainingGuessesEl.textContent = CONGRATULATIONS_MESSAGE;
-    showModal(CONGRATULATIONS_MESSAGE, randomWord);
-    return true;  // Return true to indicate a win
-  } else if (areAllLettersGuessed) {
-    remainingGuessesEl.textContent = CONGRATULATIONS_MESSAGE;
-    showModal(CONGRATULATIONS_MESSAGE, randomWord);
-    return true;  // Return true to indicate a win
+  remainingGuessesEl.textContent = isUserGuessCorrect || areAllLettersGuessed ? CONGRATULATIONS_MESSAGE : GAME_OVER_MESSAGE;
+
+  if (isUserGuessCorrect || areAllLettersGuessed) {
+    showModal(isUserGuessCorrect ? CONGRATULATIONS_MESSAGE : YOU_LOST_MESSAGE, randomWord);
+    return true;
   } else {
-    remainingGuessesEl.textContent = GAME_OVER_MESSAGE;
     showModal(YOU_LOST_MESSAGE, randomWord);
-    return false; // Return false to indicate a loss
+    return false;
   }
 }
-
-
-
 
 function showModal(message, word) {
   gameOver = true;
   modalMessage.textContent = message;
+  modalWord.textContent = message === YOU_LOST_MESSAGE
+    ? `${POKEMON_WAS_MESSAGE}${word}${BETTER_LUCK_NEXT_TIME_MESSAGE}`
+    : `${CONGRATULATIONS_MESSAGE}${YOU_CAUGHT_MESSAGE}${word}`;
 
   const gameGif = document.getElementById("gameGif");
-
-  if (message === YOU_LOST_MESSAGE) {
-    modalWord.textContent = `${POKEMON_WAS_MESSAGE}${word}${BETTER_LUCK_NEXT_TIME_MESSAGE}`;
-    gameGif.src = LOST_GIF_URL;
-  } else {
-    modalWord.textContent = `${CONGRATULATIONS_MESSAGE}${YOU_CAUGHT_MESSAGE}${word}`;
-    gameGif.src = WON_GIF_URL;
-  }
+  gameGif.src = message === YOU_LOST_MESSAGE ? LOST_GIF_URL : WON_GIF_URL;
 
   modal.style.display = "block";
 }
 
-// Function to hide the modal
 function hideModal() {
   modal.style.display = "none";
-  
 }
 
+function setDifficulty() {
+  switch (difficulty) {
+    case 1:
+      remainingGuesses = 10;
+      break;
+    case 2:
+      remainingGuesses = 7;
+      break;
+    case 3:
+    remainingGuesses = 5;
+    break;
+  default:
+    remainingGuesses = 10;
+    break;
+}
+
+remainingGuessesEl.textContent = `${GUESSES_REMAINING_MESSAGE}${remainingGuesses}`;
+}
+
+function handleDifficultyChange(event) {
+difficulty = parseInt(event.target.value, 10);
 newGame();
+}
 
-//try to make a keyboard
-//have the image of shaded pokemon like they did in the tv shows as a hint.
-
-
+function addToGuessHistory(letter) {
+const guessHistoryItem = document.createElement("span");
+guessHistoryItem.textContent = letter + " ";
+guessHistoryContainer.appendChild(guessHistoryItem);
+}
